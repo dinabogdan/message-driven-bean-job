71cef9e5142a097f044aa84d3f6aec49fec3665c
dinabogdan <bogdan.dina03@gmail.com>
2017-11-25 21:23:02 +0200

    add the message-driven-bean job-starter

---
+++ ejbModule/com/freesoft/main/JobStarter.java
@@ -0,0 +1,32 @@
+package com.freesoft.main;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+
+/**
+ * Message-Driven Bean implementation class for: JobStarter
+ */
+@MessageDriven(
+		activationConfig = { @ActivationConfigProperty(
+				propertyName = "destinationType", propertyValue = "javax.jms.Queue")
+		})
+public class JobStarter implements MessageListener {
+
+    /**
+     * Default constructor. 
+     */
+    public JobStarter() {
+        // TODO Auto-generated constructor stub
+    }
+	
+	/**
+     * @see MessageListener#onMessage(Message)
+     */
+    public void onMessage(Message message) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
